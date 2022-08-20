4a9c2829897b3c0384968b80555ad2d1d74ff977
Andrey <zhil.andrey2013@yandex.ru>
2022-08-20 16:36:01 +0500

    three commit

---
+++ Python_Task/begin/if elif/Step4.1.py
@@ -0,0 +1,78 @@
+answer = input('Какой язык программирования мы изучаем?')
+if answer == 'Python':
+    print('Верно! Мы ботаем Python =)')
+    print('Python - отличный язык!')
+else:
+    print('Не совсем так!')
+#Пароль
+a = input()
+b = input()
+if a == b:
+  print('Пароль принят')
+else:
+  print('Пароль не принят')
+#Четное или нечетное?
+n = int(input())
+if n % 2 == 0:
+    print('Четное')
+else:
+    print('Нечетное')
+#Программа должна вывести «ДА», если соотношение выполняется, и «НЕТ» — если не выполняется
+a = int(input())
+n1,n4 = (a%10000)//1000, a%10
+n2,n3 = (a%1000)//100, (a%100)//10
+print('ДА' if n1+n4==n2-n3 else "НЕТ")
+#Наименьшее из двух чисел
+a,b = int(input()),int(input())
+if a<b:
+   print(a)
+else:
+   print(b)
+
+n = a if a<b else b
+m = c if c<d else d
+print(n if n<m else m)
+#Принадлежность
+print(" Принадлежит" if -1< int(input()) < 17 else "Не принадлежит")
+
+print("Принадлежит" if a <= -3 or a >= 7 else "Не принадлежит")
+
+if (1000 <= n <= 9999) and ( n%7 == 0 or n%17 == 0):
+   print("YES")
+else:
+   print("NO")
+
+if not (a%100 == 0) and a%4 == 0 or a%400 == 0:
+  print ("YES")
+else:
+    print("NO")
+
+#Ход ладьи
+if 1 <= a == a1 <= 8 or 1 <= b == b1 <= 8:
+   print("YES")
+else:
+   print("NO")
+#step king
+if -1 <= a1 - a <= 1 and -1 <= b1 - b <= 1:
+    print("YES")
+else:
+    print("NO")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
